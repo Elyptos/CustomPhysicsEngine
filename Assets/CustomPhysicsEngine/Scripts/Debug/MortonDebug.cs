a3d2533263286d80d23c5ebfe6c4348fc8344478
Thomas Schöngrundner <thomas.schoengrundner@gmail.com>
2018-07-28 15:00:12 +0200

    Project setup and morton implementation

---
+++ Assets/CustomPhysicsEngine/Scripts/Debug/MortonDebug.cs
@@ -0,0 +1,24 @@
+﻿using CP.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CP.Debug
+{
+    public class MortonDebug : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            PrintMorton();
+        }
+
+        private void PrintMorton()
+        {
+            Morton.PrintMorton3DTablesToFile();
+            Morton.PrintMorton2DTablesToFile();
+        }
+    }
+}
+
